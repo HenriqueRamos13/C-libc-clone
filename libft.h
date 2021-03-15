b99b6dea8d764e84682ce261137b296d325a9e6c
Henrique Ramos <hramos@c1r20s3.42lisboa.com>
2021-03-15 15:55:53 +0000

    save

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hramos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 12:18:22 by hramos            #+#    #+#             */
-/*   Updated: 2021/03/15 15:45:34 by hramos           ###   ########.fr       */
+/*   Updated: 2021/03/15 15:55:32 by hramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,8 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void*));
